d56c33016e90be53b38d029c8de42afabdb576b8
Alex Stuart <alex.stuart@jisc.ac.uk>
2019-04-25 20:15:19 +0100

    Improve algorithm for generating pairwise-id
    - includes entityID for proxied IdP in generator
    - hash the generator to get a fixed length opaque part

--- pairwise_generation.py
+++ pairwise_generation.py
@@ -1,12 +1,20 @@
 import base64
+import hashlib
 
 from .base import ResponseMicroService
 
 class AddPairwiseAttribute(ResponseMicroService):
+    """
+    Class to generate pairwise-id from eduPersonTargetedID
+    pairwise-id = BASE32( SHA256( entityID of proxied IdP + '!' + opaque part of eduPersonTargetedID ) ) + '@gateway.jisc.ac.uk'
+    """
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def process(self, context, data):
         if len(data.attributes['edupersontargetedid']) > 0:
-           data.attributes.update({"pairwise-id": [base64.b32encode(bytes(data.attributes['edupersontargetedid'][0], 'utf-8')).decode() + '@gateway.jisc.ac.uk']})
+           n = hashlib.sha256()
+           n.update( bytes( data.auth_info.issuer + '!' + data.attributes['edupersontargetedid'][0], 'utf-8' ) )
+           p = base64.b32encode(n.digest()).decode() + '@gateway.jisc.ac.uk'
+           data.attributes.update({"pairwise-id": [p]})
         return super().process(context, data)
